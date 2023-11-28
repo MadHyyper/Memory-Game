8e9ebd56daba43cdd8244b041a7e3a28cdc28ae4
StealthyE <AStealthyE@gmail.com>
2023-11-27 19:43:16 -0600

    Formatting

--- memoryGame.c
+++ memoryGame.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
-	printf("Welcome to the Memory game.\n");
-	printf("To start the game press 'g'\n");
+	//add variables here
+	time_t seconds;
+	char seed[100];
+	
+	//welcome to the game
+	printf("Welcome to the Memory game.\nTo start the game press 'g'\n");
 	printf("In the game various shapes will appear with a circle around it\n");
 	printf("You must remember the shapes that are circled and repeat the order\n");
-	printf("Use w for up, s for down, d for right, a for left\n");
+	printf("Use w for up, s for down, d for right, a for left\n\n");
+	printf("Highscore Table:\nRank  Score  Name\n");
+	
+	//read and print txt file here
+	
+	
+	
+	
+	return 0;
 }
