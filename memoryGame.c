bf4ea58634a31b333401b940caf631f066191e45
malFlynn <malflynn@iastate.edu>
2023-11-27 18:47:03 -0600

    beginning
    the easy stuff

---
+++ memoryGame.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("Welcome to the Memory game.\n");
+	printf("To start the game press 'g'\n");
+	printf("In the game various shapes will appear with a circle around it\n");
+	printf("You must remember the shapes that are circled and repeat the order\n");
+	printf("Use w for up, s for down, d for right, a for left\n");
+}
